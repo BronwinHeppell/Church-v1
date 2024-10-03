a1b1bf32aff690e383b6f51fe5c018c2e777bffc
Bronwin Heppell <bronwinheppell98@gmail.com>
2024-10-03 15:18:29 +0200

    Update image location for static images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,43 +10,49 @@ import MissionStatement from './sections/mission-statement';
 import Services from './sections/services';
 
 const Home: React.FC = () => {
-  return (
-    <>
-      <Head>
-        <title>Corpus Christi Anglican Church | Worship in [Garsfontein]</title>
-        <meta
-          name="description"
-          content="Welcome to Corpus Christi Anglican Church, offering worship services, community outreach, and more in Garsfontein. We are a community of believers who are passionate about serving God and our neighbours."
-        />
-        <meta name="keywords" content="Anglican Church, Garsfontein, Worship, Community Outreach" />
-        <meta property="og:title" content="Corpus Christi Anglican Church" />
-        <meta property="og:description" content="Welcome to Corpus Christi Anglican Church, offering worship services, community outreach, and more in Garsfontein." />
-        <meta property="og:image" content="/assets/hero.jpg" />
-        <meta name="twitter:title" content="Corpus Christi Anglican Church" />
-        <meta name="twitter:description" content="Welcome to Corpus Christi Anglican Church, offering worship services, community outreach, and more in Garsfontein." />
-        <meta name="twitter:image" content="/assets/hero.jpg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>Corpus Christi Anglican Church | Worship in [Garsfontein]</title>
+				<meta
+					name="description"
+					content="Welcome to Corpus Christi Anglican Church, offering worship services, community outreach, and more in Garsfontein. We are a community of believers who are passionate about serving God and our neighbours."
+				/>
+				<meta name="keywords" content="Anglican Church, Garsfontein, Worship, Community Outreach" />
+				<meta property="og:title" content="Corpus Christi Anglican Church" />
+				<meta
+					property="og:description"
+					content="Welcome to Corpus Christi Anglican Church, offering worship services, community outreach, and more in Garsfontein."
+				/>
+				<meta property="og:image" content="/static/hero.jpg" />
+				<meta name="twitter:title" content="Corpus Christi Anglican Church" />
+				<meta
+					name="twitter:description"
+					content="Welcome to Corpus Christi Anglican Church, offering worship services, community outreach, and more in Garsfontein."
+				/>
+				<meta name="twitter:image" content="/static/hero.jpg" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
 
-      <Navbar />
+			<Navbar />
 
-      <Hero />
+			<Hero />
 
-      <main className="mx-auto px-5 text-gray-800 md:max-w-4xl md:px-10">
-        <Services />
+			<main className="mx-auto px-5 text-gray-800 md:max-w-4xl md:px-10">
+				<Services />
 
-        <MissionStatement />
+				<MissionStatement />
 
-        <AboutUs />
+				<AboutUs />
 
-        <Events />
+				<Events />
 
-        <FAQ />
-      </main>
+				<FAQ />
+			</main>
 
-      <Footer />
-    </>
-  );
+			<Footer />
+		</>
+	);
 };
 
 export default Home;
--- src/app/sections/footer.tsx
+++ src/app/sections/footer.tsx
@@ -5,95 +5,102 @@ import { merriweather } from '@/shared/core/fonts';
 import Link from 'next/link';
 
 const ContactDetails = () => {
-  const showInMapClicked = () => {
-    window.open(
-      'https://www.google.com/maps/place/Corpus+Christi+Anglican+Church/@-25.7961862,28.2931054,17z/data=!3m1!4b1!4m6!3m5!1s0x1e955ffb3657c89b:0x84e228c1cd662421!8m2!3d-25.7961862!4d28.2956857!16s%2Fg%2F11cs18f00p?entry=ttu',
-    );
-  };
+	const showInMapClicked = () => {
+		window.open(
+			'https://www.google.com/maps/place/Corpus+Christi+Anglican+Church/@-25.7961862,28.2931054,17z/data=!3m1!4b1!4m6!3m5!1s0x1e955ffb3657c89b:0x84e228c1cd662421!8m2!3d-25.7961862!4d28.2956857!16s%2Fg%2F11cs18f00p?entry=ttu',
+		);
+	};
 
-  return (
-    <div className="w-full p-4 flex flex-col space-y-2">
-      <div>
-        <p className={`${merriweather.className}`}>Address:</p>
-        <p onClick={() => showInMapClicked()} className="cursor-pointer rounded-lg hover:scale-[110%] transition transform duration-200 ease-out">
-          482 De Bron Rd, Garsfontein, Pretoria, 0042
-        </p>
-      </div>
-      <div>
-        <p className={`${merriweather.className}`}>Phone: </p>
-        <p onClick={() => window.open('tel:0129935161')} className="cursor-pointer rounded-lg hover:scale-[110%] transition transform duration-200 ease-out">
-          (012) 993 5161</p>
-      </div>
-      <div>
-        <p className={`${merriweather.className}`}>Email:</p>
-        <a href='mailto:garsfontein.acsa@gmail.com' target="_blank"
-        >
-          <button className="cursor-pointer rounded-lg hover:scale-[110%] transition transform duration-200 ease-out">
-            garsfontein.acsa@gmail.com
-          </button>
-        </a>
-      </div >
-    </div >
-  )
-}
+	return (
+		<div className="flex w-full flex-col space-y-2 p-4">
+			<div>
+				<p className={`${merriweather.className}`}>Address:</p>
+				<p
+					onClick={() => showInMapClicked()}
+					className="transform cursor-pointer rounded-lg transition duration-200 ease-out hover:scale-[110%]"
+				>
+					482 De Bron Rd, Garsfontein, Pretoria, 0042
+				</p>
+			</div>
+			<div>
+				<p className={`${merriweather.className}`}>Phone: </p>
+				<p
+					onClick={() => window.open('tel:0129935161')}
+					className="transform cursor-pointer rounded-lg transition duration-200 ease-out hover:scale-[110%]"
+				>
+					(012) 993 5161
+				</p>
+			</div>
+			<div>
+				<p className={`${merriweather.className}`}>Email:</p>
+				<a href="mailto:garsfontein.acsa@gmail.com" target="_blank">
+					<button className="transform cursor-pointer rounded-lg transition duration-200 ease-out hover:scale-[110%]">
+						garsfontein.acsa@gmail.com
+					</button>
+				</a>
+			</div>
+		</div>
+	);
+};
 
 const Socials = () => {
-  return (
-    <div className="w-full text-center flex flex-col justify-center items-center">
-      <div className="flex flex-col items-center">
-        <Image
-          src="assets/logo/logo_small.svg"
-          alt="Small Logo"
-          className=""
-          width={150}
-          height={150}
-        />
-        <p>Join us for worship every Sunday at Corpus Christi</p>
-        <div className='flex justify-center'>
-          <Link href="">
-            <Image
-              src="/assets/icons/instagram.svg"
-              alt="Instagram Image"
-              width={20}
-              height={0}
-              className='cursor-pointer m-3 object-contain hover:scale-110 transition transform duration-200 ease-out'
-            /></Link>
-          <Link href="">
-            <Image
-              src="/assets/icons/facebook.svg"
-              alt="Facebook Image"
-              width={20}
-              height={0}
-              className='cursor-pointer m-3 object-contain hover:scale-110 transition transform duration-200 ease-out'
-            /></Link>
-          <Link href="">
-            <Image
-              src="/assets/icons/whatsapp.svg"
-              alt="Whatsapp Image"
-              width={20}
-              height={0}
-              className='cursor-pointer m-3 object-contain hover:scale-110 transition transform duration-200 ease-out'
-            /></Link>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-
+	return (
+		<div className="flex w-full flex-col items-center justify-center text-center">
+			<div className="flex flex-col items-center">
+				<Image
+					src="/static/logo/logo_small.svg"
+					alt="Small Logo"
+					className=""
+					width={150}
+					height={150}
+				/>
+				<p>Join us for worship every Sunday at Corpus Christi</p>
+				<div className="flex justify-center">
+					<Link href="">
+						<Image
+							src="/static/icons/instagram.svg"
+							alt="Instagram Image"
+							width={20}
+							height={0}
+							className="m-3 transform cursor-pointer object-contain transition duration-200 ease-out hover:scale-110"
+						/>
+					</Link>
+					<Link href="">
+						<Image
+							src="/static/icons/facebook.svg"
+							alt="Facebook Image"
+							width={20}
+							height={0}
+							className="m-3 transform cursor-pointer object-contain transition duration-200 ease-out hover:scale-110"
+						/>
+					</Link>
+					<Link href="">
+						<Image
+							src="/static/icons/whatsapp.svg"
+							alt="Whatsapp Image"
+							width={20}
+							height={0}
+							className="m-3 transform cursor-pointer object-contain transition duration-200 ease-out hover:scale-110"
+						/>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 const Footer = () => {
-  return (
-    <footer id="Footer" className="bg-[#2B2B2B] p-4 text-center text-white">
-      <div className="container flex flex-col gap-4 items-center justify-center sm:flex-row sm:justify-evenly">
-        <ContactDetails />
-        <Socials />
-        <Map />
-      </div>
-      <hr className="my-4" />
-      <p className="text-center sm:text-end">&copy; 2024 Corpus Christi Anglican Church </p>
-    </footer>
-  );
+	return (
+		<footer id="Footer" className="bg-[#2B2B2B] p-4 text-center text-white">
+			<div className="container flex flex-col items-center justify-center gap-4 sm:flex-row sm:justify-evenly">
+				<ContactDetails />
+				<Socials />
+				<Map />
+			</div>
+			<hr className="my-4" />
+			<p className="text-center sm:text-end">&copy; 2024 Corpus Christi Anglican Church </p>
+		</footer>
+	);
 };
 
 export default Footer;
