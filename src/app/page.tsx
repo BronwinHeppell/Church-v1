29795cae65ffe161c175867231c18b5c299b97c8
Bronwin Heppell <bronwinheppell98@gmail.com>
2024-10-04 09:23:56 +0200

    Update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,28 +12,6 @@ import Services from './sections/services';
 const Home: React.FC = () => {
 	return (
 		<>
-			<Head>
-				<title>Corpus Christi Anglican Church | Worship in [Garsfontein]</title>
-				<meta
-					name="description"
-					content="Welcome to Corpus Christi Anglican Church, offering worship services, community outreach, and more in Garsfontein. We are a community of believers who are passionate about serving God and our neighbours."
-				/>
-				<meta name="keywords" content="Anglican Church, Garsfontein, Worship, Community Outreach" />
-				<meta property="og:title" content="Corpus Christi Anglican Church" />
-				<meta
-					property="og:description"
-					content="Welcome to Corpus Christi Anglican Church, offering worship services, community outreach, and more in Garsfontein."
-				/>
-				<meta property="og:image" content="/static/hero.jpg" />
-				<meta name="twitter:title" content="Corpus Christi Anglican Church" />
-				<meta
-					name="twitter:description"
-					content="Welcome to Corpus Christi Anglican Church, offering worship services, community outreach, and more in Garsfontein."
-				/>
-				<meta name="twitter:image" content="/static/hero.jpg" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-			</Head>
-
 			<Navbar />
 
 			<Hero />
