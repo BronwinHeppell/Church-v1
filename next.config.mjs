30304142b873ca0f638c36b6bcd4711975cbaca3
Bronwin Heppell <bronwinheppell98@gmail.com>
2024-10-04 09:23:56 +0200

    Update metadata

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
 	reactStrictMode: true,
 	trailingSlash: true,
 	images: {
-		unoptimized: true,
 		remotePatterns: [
 			{
 				protocol: 'https',
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,38 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: 'Corpus Christi Anglican Church',
+	title: 'Corpus Christi Anglican Church | Worship in Garsfontein',
+	description:
+		'Welcome to Corpus Christi Anglican Church, offering worship services, community outreach, and more in Garsfontein. We are a community of believers who are passionate about serving God and our neighbours.',
+	keywords: ['Anglican Church', 'Garsfontein', 'Worship', 'Community Outreach'],
+	openGraph: {
+		title: 'Corpus Christi Anglican Church',
+		description:
+			'Welcome to Corpus Christi Anglican Church, offering worship services, community outreach, and more in Garsfontein.',
+		images: [
+			{
+				url: '/static/hero.jpg',
+			},
+		],
+	},
+	twitter: {
+		title: 'Corpus Christi Anglican Church',
+		description:
+			'Welcome to Corpus Christi Anglican Church, offering worship services, community outreach, and more in Garsfontein.',
+		images: ['/static/hero.jpg'],
+	},
+	viewport: 'width=device-width, initial-scale=1',
+	authors: [
+		{ name: 'Corpus Christi Anglican Church', url: 'https://www.corpus-christi-garsfontein.org' },
+	],
+	robots: {
+		index: true,
+		follow: true,
+	},
+	icons: {
+		shortcut: '/favicon.ico',
+	},
+	category: 'Worship',
 };
 
 export default function RootLayout({
